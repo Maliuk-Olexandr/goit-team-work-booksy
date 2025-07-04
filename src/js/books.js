7efeec38bd77f7ff11f57bf8711b31298393b1cc
Danylo Borovskyi <danilborovskii199911@gmail.com>
2025-07-04 10:19:08 +0300

    create-mark-up early stages

--- src/js/books.js
+++ src/js/books.js
@@ -1,5 +1,3 @@
-import axios from "axios"
-
 const gallery = document.querySelector(".gallery");
 import {getTopBooks, getBooksByCategory} from "./products-api"
 
@@ -8,28 +6,40 @@ const booksByCategory = await getBooksByCategory("Trade Fiction Paperback")
 
 console.log(booksByCategory);
 
+createMarkupByCategory(booksByCategory)
+// createMarkup(topBooks);
 
-console.log("test", topBooks);
+// function createMarkup(data) {
+//   const markup = data.flatMap(({ books }) =>
+//     books.map(book => {
+//       return `
+//         <li>
+//           <img class="book-cover" src="${book.book_image}" alt="${book.title}" width="150" />
+//           <h3>${book.title}</h3>
+//           <p>by ${book.author}</p>
+//           <a href="${book.amazon_product_url}" target="_blank">Buy on Amazon</a>
+//         </li>
+//       `;
+//     })
+//   ).join('');
 
-createMarkup(topBooks);
+//   return markup;
+// }
 
-function createMarkup(data) {
-  const markup = data.flatMap(({ books }) =>
-    books.map(book => {
-        console.log(book);
-        
-      return `
+function createMarkupByCategory(data) {
+  const markup = data.map(({ title, author, book_image, price }) => `
         <li>
-          <img class="book-cover" src="${book.book_image}" alt="${book.title}" width="150" />
-          <h3>${book.title}</h3>
-          <p>by ${book.author}</p>
-          <a href="${book.amazon_product_url}" target="_blank">Buy on Amazon</a>
+          <img class="book-cover" src="${book_image}" alt="${title}" width="150" />
+          <h3>${title}</h3>
+          <p>${author}</p>
+          <p>${price}</p>
+          <button>Learn more</button>
         </li>
-      `;
-    })
+      `
   ).join('');
 
   return markup;
 }
 
-gallery.insertAdjacentHTML("beforeend", createMarkup(topBooks))
+
+gallery.insertAdjacentHTML("beforeend", createMarkupByCategory(booksByCategory))
