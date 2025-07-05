723c4098c1433505382fd0d1a2dcfa09e17ac0bb
OksanaSemenchuk <oksana.kostenko3000@gmail.com>
2025-07-05 16:28:45 +0300

    wrote JS-code for the section hero

--- src/main.js
+++ src/main.js
@@ -10,3 +10,9 @@ import './js/modal.js';
 import { testAPI } from './js/products-api.js';
 testAPI();
 // @TODO remove before production deployment
+
+import { initHeroSlider } from './js/hero.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  initHeroSlider();
+});
